e35e2bb00c1bc57f78ea4834ad0bdcb56755e784
kporcelain <zhukovaksusha@gmail.com>
2017-10-26 14:35:37 +0300

    updated names and usage of modules

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,42 +1,32 @@
 import re
-import collections
+from collections import Counter
 
 
-def load_data():
-    filename = input("Enter path to the file: ")
-    open_text = open(filename, "r", encoding="utf8")
-    full_text_from_input = list()
-    for words in open_text:
-        full_text_from_input.append(words.strip())
-    return full_text_from_input
+def load_data_from_file():
+    with open(input(), "r", encoding="utf8") as f:
+        text = f.readlines()
+        return text
 
 
-def formating(text):
-    text = str(text)
-    text = text.lower()
-    pattern = re.compile('\w+')
-    text = pattern.findall(text)
-    return text
+def lowercase_split_strip_file(text):
+    text = str(text).strip().lower()
+    text_after_formatting = re.findall(r'\w+', text)
+    for words in range(text_after_formatting.count("n")):
+        if "n" in text_after_formatting:
+            text_after_formatting.remove("n")
+    return text_after_formatting
 
 
-def print_list(register):
-    for word in register:
+def get_ten_frequent_words(text):
+    text_to_list = list(lowercase_split_strip_file(text))
+    dict_of_words = Counter(text_to_list)
+    return Counter(dict_of_words).most_common(10)
+
+
+def print_list_of_frequent_words_in_column(list_of_frequent_words):
+    for word in list_of_frequent_words:
         print(word[0], "-", word[1])
 
 
-def get_most_frequent_words(text):
-    dict_of_words = collections.Counter()
-    for word in formating(text):
-        dict_of_words[word] += 1
-
-    register_of_words = list()
-
-    for words in dict_of_words:
-        register_of_words.append((dict_of_words[words], words))
-
-    register_of_words = sorted(register_of_words)
-    register_of_words = sorted(register_of_words, key=lambda x: x[0], reverse=True)[:10]
-    return print_list(register_of_words)
-
-
-print(get_most_frequent_words(load_data()), sep="\n")
+if __name__ == '__main__':
+    print(print_list_of_frequent_words_in_column(get_ten_frequent_words(load_data_from_file())), sep="\n")
