913237d3cdcac295e053b6b4154aa778401ea861
kporcelain <zhukovaksusha@gmail.com>
2017-11-01 14:51:01 +0300

    updated functions, changed order

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,17 +1,16 @@
 import re
-import sys
 from collections import Counter
-
-filename = ""
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
+import sys
 
 
-def load_data_lowercase_remove_punctuation():
-    with open(filename, "r", encoding="utf8") as f:
-        text = f.read().lower()
+def load_data(filepath):
+    with open(filepath, "r", encoding="utf8") as f:
+        text = f.read()
+    return text
+
+
+def lowercase_text_remove_punctuation(text):
+    text = text.lower()
     text_without_punctuation = re.findall(r'\w+', text)
     return text_without_punctuation
 
@@ -26,6 +25,8 @@ def print_list_of_frequent_words_in_column(list_of_frequent_words):
     for word, number in list_of_frequent_words:
         print(word, "-", number)
 
+if __name__ == '__main__':
+     if len(sys.argv) > 1:
+        filepath = sys.argv[1]
 
-print(print_list_of_frequent_words_in_column(get_ten_frequent_words(load_data_lowercase_remove_punctuation())),
-      sep="\n")
+print_list_of_frequent_words_in_column(get_ten_frequent_words(lowercase_text_remove_punctuation(load_data(filepath))))
