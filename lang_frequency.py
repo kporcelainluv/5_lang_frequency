3ae36c00466baaefe07fd8ad56a8bb0d3232cfcf
kporcelain <zhukovaksusha@gmail.com>
2017-11-08 14:32:57 +0300

    updated names of functions

--- lang_frequency.py
+++ lang_frequency.py
@@ -9,23 +9,23 @@ def load_data(filepath):
     return text
 
 
-def remove_punctuation(text):
+def split_text_remove_punctuation(text):
     text_without_punctuation = re.findall(r'\w+', text.lower())
     return text_without_punctuation
 
 
-def get_frequent_words(text):
+def get_frequent_words(list_of_words):
     number_of_frequent_words = 10
-    counter_of_words = Counter(text)
+    counter_of_words = Counter(list_of_words)
     return counter_of_words.most_common(number_of_frequent_words)
 
 
-def print_list_of_frequent_words_in_column(list_of_frequent_words):
-    for word, number in list_of_frequent_words:
+def print_frequent_words_in_column(frequent_words):
+    for word, number in frequent_words:
         print(word, "-", number)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         filepath = sys.argv[1]
-    print_list_of_frequent_words_in_column(get_frequent_words(remove_punctuation(load_data(filepath))))
+    print_frequent_words_in_column(get_frequent_words(split_text_remove_punctuation(load_data(filepath))))
